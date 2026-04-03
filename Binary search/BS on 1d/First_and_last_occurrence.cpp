7aaa919d28fc4dda408ddbf16048aa34c135082e
summi-rahman <tahseenanwer100@gmail.com>
2026-04-03 10:15:00 +0530

    First and last occurrence

--- Binary search/BS on 1d/First_and_last_occurrence.cpp
+++ Binary search/BS on 1d/First_and_last_occurrence.cpp
@@ -1,28 +1,54 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int first_and_last_occurrence(vector<int>arr,int n,int target,int low ,int high){
-  while(low<=high){
-    int mid = (low+high)/2;
-    if(arr[mid] == target) { return mid;}
-    else if( mid < target) {return first_and_last_occurrence(arr,n,target,mid+1,high);}
-    else {return first_and_last_occurrence(arr,n,target,low,mid-1);}
-  }
-  return -1;
+
+int lower_bound(vector<int>arr,int n, int target){
+     int low = 0;
+    int high = n - 1;
+    int ans = n; 
+    while (low <= high) {
+        int mid = low + (high - low) / 2; 
+
+        if (arr[mid] >= target) {
+            ans = mid;       
+            high = mid - 1; 
+        } else {
+            low = mid + 1;   
+        }
+    }
+    return ans;
 }
-int main(){
- int n;
- cin>>n;
- vector<int>arr;
- for( int i=0;i<n;i++){
+int upper_bound(vector<int>arr,int n, int target){
+    int low = 0, high = n - 1;
+   int ans = 0;
+   while(low<=high){
+    int mid = (low+high)/2;
+    if(arr[mid]>target){
+       ans = mid;
+        high = mid-1;
+     
+    }
+    else{
+       low = mid+1;
+    }
+   }
+   return ans;
+}
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> arr;
+  for (int i = 0; i < n; i++)
+  {
     int x;
-    cin>>x;
-    arr.push_back(x); 
- }
+    cin >> x;
+    arr.push_back(x);
+  }
   int target;
-  cin>>target;
-  int low = 0,high = n-1;
-  int ans = first_and_last_occurrence(arr,n,target,low,high);
-  cout<<ans;
+  cin >> target;
+  int first = lower_bound(arr,n,target);
+  int last = upper_bound(arr,n,target) - 1 ;
+  cout<<first<<" " << last ;
   return 0;
 }
