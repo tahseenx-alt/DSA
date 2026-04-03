d3f6448d74ae86cf5a28e8513f9c397bfbb2ca59
summi-rahman <tahseenanwer100@gmail.com>
2026-04-03 18:42:57 +0530

    Count the Occurrences of a specific number in a array

--- Binary search/BS on 1d/Count_Occurrences.cpp
+++ Binary search/BS on 1d/Count_Occurrences.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+int first_occurrence(vector<int>arr,int n,int target){
+  int first = -1;
+  int low =0,high = n-1;
+  while(low<=high){
+    int mid = (low+high)/2;
+    if(arr[mid]==target){
+       first = mid;
+       high = mid-1;
+    }
+    else if(arr[mid]<target){
+      low = mid+1;
+    }
+    else{
+      high = mid-1;
+    }
+  }
+  return first;
+}
+int last_occurrence(vector<int>arr,int n, int target){
+  int last = first_occurrence(arr,n,target);
+  int low = 0,high = n-1;
+  while(low<=high){
+    int mid = (low+high)/2;
+    if(arr[mid]==target){
+       last = mid;
+       low = mid+1;
+    }
+    else if(arr[mid]<target){
+      low = mid+1;
+    }
+    else{
+      high = mid-1;
+    }
+  }
+  return last;
+}
 int main()
 {
   int n;
@@ -14,12 +50,9 @@ int main()
   }
   int target;
   cin >> target;
-   int count =0;
-  for( int i =0; i<n;i++){
-    if(arr[i] == target){
-        count++;
-    }
-  }
-   cout<<count;
+  int first = first_occurrence(arr,n,target);
+  int last = last_occurrence(arr,n,target);
+  int count = last - first +1;
+  cout<<count;
   return 0;
 }
