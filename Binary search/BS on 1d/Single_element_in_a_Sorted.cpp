991f052b4aa66d19462fd0a6ef94312ea805042f
summi-rahman <tahseenanwer100@gmail.com>
2026-04-08 20:17:04 +0530

    Single element in a Sorted Array

---
+++ Binary search/BS on 1d/Single_element_in_a_Sorted.cpp
@@ -0,0 +1,23 @@
+//Find out how many times the array has been rotated
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> arr;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+    
+    return 0;
+}
+
+
+
