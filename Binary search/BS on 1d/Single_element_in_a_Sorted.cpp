26954392a20a2dfe13f7ed2bb048f3e802779a9a
summi-rahman <tahseenanwer100@gmail.com>
2026-04-09 20:21:48 +0530

    Single element in a Sorted Array

--- Binary search/BS on 1d/Single_element_in_a_Sorted.cpp
+++ Binary search/BS on 1d/Single_element_in_a_Sorted.cpp
@@ -3,7 +3,25 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+int search_element(vector<int>arr,int n){
+    if(n==1) return arr[0];
+    else if(arr[0] != arr[1]) return arr[0];
+    else if(arr[n-1] != arr[n-2]) return arr[n-1];
+    int low = 1,high=n-2;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid] != arr[mid-1] && arr[mid]!= arr[mid+1]){
+            return arr[mid];
+        }
+        if((mid%2 ==1 && arr[mid] == arr[mid-1]) || (mid%2 == 0 && arr[mid] == arr[mid+1])){
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return -1;
+}
 
 int main() {
     int n;
@@ -15,6 +33,8 @@ int main() {
         cin >> x;
         arr.push_back(x);
     }
+    int ans = search_element(arr,n);
+    cout<<ans;
     
     return 0;
 }
