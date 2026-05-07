abfb7185394757dd283ffb61da8ca93645b1b8f9
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-05-07 19:30:26 +0530

    Merge Sort

---
+++ Sorting/mergesort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    for( int x:arr){
+        cout<<x<<" ";
+    }
+    return 0;
+}
