3889a1b64fee13b8a0c4b6a5e252879eac26c63e
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-05-14 20:56:46 +0530

    Longest Common Prefix in a string

--- LinkedList/1D Linked List/Convert_array_to_LinkedList.cpp
+++ LinkedList/1D Linked List/Convert_array_to_LinkedList.cpp
@@ -22,7 +22,7 @@ Node* Convertarr2li(vector<int>&arr){
  
     Node * head = new Node(arr[0]);
     Node * mover = head;
-    for( int i =1;i<arr.size();i++){
+    for( int i =0;i<arr.size();i++){
         Node * temp = new Node(arr[i]);
         mover->next = temp;
         mover = temp;
