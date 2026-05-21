6dcb2027c7c4162c6bf6bd87b56a875db5feb81b
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-05-21 20:13:44 +0530

    Inserting a new element at the kth position

---
+++ LinkedList/1D Linked List/insert_at_kth_element.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1 , Node * next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data2 ){
+        data = data2;
+        next = nullptr;
+    }
+};
+Node * convert(vector<int>&arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for( int i =1;i<arr.size(); i++){
+        Node * temp = new Node(arr[i]);
+        mover->next=temp;
+        mover = temp;
+    }
+    return head;
+}
+
+
+void print(Node * head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+}
+int main(){
+
+    vector<int>arr = {5,6,1,3,2};
+    Node* head = convert(arr);
+    int k;
+    cin>>k;
+    return 0;
+}
