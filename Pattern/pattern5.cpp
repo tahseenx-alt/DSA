d1ba992b442e8aa1002b7022b880072c9e82d58d
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-07-31 18:19:29 +0530

    Pattern question 5

--- Pattern/pattern5.cpp
+++ Pattern/pattern5.cpp
@@ -4,8 +4,8 @@ int main(){
     int n;
     cin>>n;
     for(int i = 1; i<=n; i++){
-        for(int j=i ; j<=n; j++){
-            cout<< i << "  " ;
+        for(int j=n ; j>=i; j--){
+            cout<< " * " ;
         }
         cout<<endl;
     }
