df014927c055487690a7159d17d560886e4e8b1e
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-08-03 12:38:18 +0530

    Pattern question 13

--- Pattern/pattern13.cpp
+++ Pattern/pattern13.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    int s =1;
     for(int i=1; i<=n; i++){
         for( int j=1; j<=i; j++){
-            cout<<j;
+            cout<<s<<" ";
+            s++;
         }
         cout<<endl;
     }
