ce25a7af9a520ad8e0ce402904c02d76e95cb2d5
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-08-03 12:52:35 +0530

    Pattern question 16

---
+++ Pattern/pattern16.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i =0;i<n; i++){
+        char ch = 'A' + i;
+        for(int j = 0;j<=i; j++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
