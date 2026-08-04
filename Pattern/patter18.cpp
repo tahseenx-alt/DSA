3349ddc52fdb8b0335c367b5a08a9edfffa854f3
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-08-04 21:35:21 +0530

    Pattern question 18

---
+++ Pattern/patter18.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i =1; i<=n; i++){
+        for(char ch = 'A'+n-1; ch>='A'; ch--){
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
