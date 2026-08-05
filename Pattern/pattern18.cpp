f95cdbf3b748bd2ae660b5bb9e6f0a47d5e9e263
Tahseen Anwer <tahseenanwer100@gmail.com>
2026-08-05 20:08:12 +0530

    Pattern question 18

--- Pattern/pattern18.cpp
+++ Pattern/pattern18.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i =1; i<=n; i++){
-        for(char ch = 'A'+n-1; ch>='A'; ch--){
-            
-        }
+    for(int i = 0; i<n; i++){
+        for(char ch = 'E'-i; ch<='E'; ch++){
+            cout<<ch<<" ";
+         }
         cout<<endl;
     }
     return 0;
